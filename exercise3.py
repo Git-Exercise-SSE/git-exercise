a0211c9851eff59ceaf945d065a01f07131026e1
Jun Zhang <98931091+jzhang306@users.noreply.github.com>
2025-10-28 20:40:31 +0100

    Refactor: Remove duplicate

--- exercise3.py
+++ exercise3.py
@@ -24,16 +24,12 @@ class Rectangle:
     @property
     def upper_right(self) -> Point2D:
         return self.corner(3)
-
+      
     def contains(self, point: Point2D, tolerance: float = 0.0) -> bool: # Task B
-    # def contains(self, point: Point2D) -> bool:
-        # Task A: remove duplication by defining a function
-        #         that checks if a value is within an interval
-        #         and reuse that here.
         ll_px = point.x - self._lower_left.x
         ll_py = point.y - self._lower_left.y
-        return ll_px >= - tolerance and ll_px <= self._dx + tolerance \
-            and ll_py >= - tolerance and ll_py <= self._dy + tolerance
+        return self._is_in_interval(ll_px, -tolerance, self._dx + tolerance) \
+            and self._is_in_interval(ll_py, -tolerance, self._dy + tolerance)
 
     def _is_idx_on_upper_edge(self, i: int) -> bool:
         return i in [2, 3]
@@ -41,7 +37,9 @@ class Rectangle:
     def _is_idx_on_right_edge(self, i: int) -> bool:
         return i in [1, 3]
 
-    # def is_in_interval(...) -> bool: # Task A
+    @staticmethod
+    def _is_in_interval(value: float, start: float, end: float) -> bool:
+        return start <= value <= end
 
 
 def test_rectangle_contains_exact() -> None:
